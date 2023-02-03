e92d4c6fd7e861594ce366e10346208f3c7f90e0
mnaqeeb06 <muhammadnaqeebawan@gmail.com>
2023-02-03 22:42:35 +0500

    Firebase configuration in project

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_authentication/firebase_options.dart';
 import 'package:firebase_authentication/screens/login_Screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/login_email_password_screen.dart';
 import 'screens/phone_screen.dart';
 import 'screens/signup_email_password_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
